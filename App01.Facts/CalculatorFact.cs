85eabe2ac7047b1b238f3bf31bed5c12805b1b7d
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2016-11-09 14:08:01 +0700

    Initial commit
    First commit the code that use to add two numbers as string.

---
+++ App01.Facts/CalculatorFact.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace App01.Facts {
+  public class CalculatorFact {
+
+    public class AddMethod : IDisposable {
+
+      private Calculator c;
+      private readonly ITestOutputHelper _output;
+       
+      // Setup
+      public AddMethod(ITestOutputHelper output) {
+        c = new Calculator();
+        _output = output;
+      }
+
+      [Theory]
+      [InlineData("1", "1", "2")]
+      [InlineData("2", "2", "4")]
+      //[MemberData("OneDigitInputs", 10)]
+      public void OneDigit(string v1, string v2, string expected) { 
+        var result = c.Add(v1, v2);
+
+        _output.WriteLine($"{v1} + {v2} = {result}");
+        Assert.Equal(expected, result);
+      }
+
+      public static IEnumerable<Object[]> OneDigitInputs(int count) {
+        for (int i = 0; i < count; i++) {
+          yield return new object[] {
+            i.ToString(), i.ToString(), i.ToString()
+          };
+        }
+      }
+
+      //[Fact]
+      //public void OneDigit_2() {  
+      //  var result = c.Add("2", "2"); 
+      //  Assert.Equal("4", result);
+      //}
+
+      [Fact]
+      public void NegativeValues() { 
+        var result = c.Add("-5", "8");
+        Assert.Equal("3", result);
+      }
+
+      [Fact]
+      public void EmptyStringOrNull_TreatAsZero() { 
+        var result1 = c.Add(null, "9");
+        var result2 = c.Add("", "9");
+        var result3 = c.Add("9", null);
+        var result4 = c.Add("9", "");
+
+        Assert.Equal("9", result1);
+        Assert.Equal("9", result2);
+        Assert.Equal("9", result3);
+        Assert.Equal("9", result4);
+      }
+
+      // [Fact]
+      // public void TwoDigits() { 
+      //   var result = c.Add("11", "22");
+      //   Assert.Equal("33", result);
+      // }
+     
+
+      [Fact]
+      public void OnlyDigitCharacterIsAllowed() {
+        var ex = Assert.Throws<ArgumentException>(() => {
+
+          c.Add("1a", "2");
+
+        });
+
+        Assert.Equal("value1", ex.ParamName);
+      }
+
+      // Teardown
+      public void Dispose() {
+        c = null; 
+      }
+    }
+  }
+}
---
+++ App01/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01 {
+  public class Calculator {
+
+    public string Add(string value1,
+                      string value2) {
+
+      if (string.IsNullOrEmpty(value1)) value1 = "0";
+      if (string.IsNullOrEmpty(value2)) value2 = "0";
+
+      if (HasNonDigits(value1)) {
+        throw new ArgumentException("value cannot contains non digit characters", 
+                                    nameof(value1));
+      }
+
+      if (HasNonDigits(value2)) {
+        throw new ArgumentException("value cannot contains non digit characters",
+                                    nameof(value2));
+      }
+
+      int sign1 = value1[0] == '-' ? -1 : 1;
+      int sign2 = value2[0] == '-' ? -1 : 1;
+
+      char ch1 = value1[value1.Length - 1];
+      char ch2 = value2[value2.Length - 1];
+
+      int v1 = (int)(ch1 - '0') * sign1;
+      int v2 = (int)(ch2 - '0') * sign2;
+
+      return (v1 + v2).ToString();
+    }
+
+    private bool HasNonDigits(string value) {
+      foreach(char ch in value) {
+        if (!Char.IsDigit(ch) && ch != '-') return true;
+      }
+      return false;
+    }
+  }
+}
